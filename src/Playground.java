2509bf5d09894d073248002ec6d18a248b4e01ef
Aleksander Grzybowski <kelog@orange.pl>
2014-10-23 19:11:15 +0200

    Deleting Playground.java from repo

--- src/Playground.java
+++ src/Playground.java
@@ -6,8 +6,13 @@ public class Playground {
 //		double result = e.setVariable("x", 0).evaluate();
 //		System.out.println(result);
 
-		double result = new Integrator().integrateASM(-10, 10, 1000, "1");
-		System.out.printf("%.3f", result);
+		double result;
+
+		result = new Integrator().integrateASM(0, Math.PI, 100, "sin(x)");
+		System.out.printf("" + result);
+
+		result = new Integrator().integrateASM(0, Math.PI, 100, "sin(x)");
+		System.out.printf("" + result);
 
 
 
