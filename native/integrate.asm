ae48d9956fc40333cd5e6f7204b2db5010b83a39
Aleksander Grzybowski <kelog@orange.pl>
2014-10-23 16:06:00 +0200

    Fixed asm, working integration

--- native/integrate.asm
+++ native/integrate.asm
@@ -1,20 +1,72 @@
-global  integrateASM
+global integrateASM:function
 global testASMLibrary:function
-
-
-integrateASM:
-        push    ebp
-        mov     ebp,esp
-        sub     esp,0x40        ; 64 bytes of local stack space
-        mov     ebx,[ebp+8]     ; first parameter to function
-
-        ; some more code
-
-        leave                   ; mov esp,ebp / pop ebp
-        ret
-
-
+section .text
 
 testASMLibrary:
     mov eax, 1337
     ret
+
+
+
+; [esp+4] = left
+; [esp+12] = right
+; [esp+20] = points
+; [esp+24] = tab
+
+dwa: dq 2.0
+
+integrateASM:
+
+    push eax
+    push ecx
+    push esi
+
+    ;fld qword [dwa]
+    ;ret
+
+	mov ecx, [esp+20+12]
+
+
+	fld1
+	fld1
+	fsub ; 0 w st(0)
+
+	; wartości krańcowe
+	mov esi, [esp+24+12]
+	fld qword [esi]
+	fadd
+	shl ecx, 3 ; bo double
+	add esi, ecx
+	shr ecx, 3
+	fld qword [esi+12]
+	fadd
+
+
+
+	mov ecx, [esp+20+12]
+	dec ecx
+	mov esi, [esp+24+12]
+	add esi, 8  ;pomijamy a[0] i a[n]
+
+
+petla:
+	fadd qword [esi]
+	fadd qword [esi]
+	add esi, 8
+	loop petla
+
+	; w st(0) jest wynik przed pomnożeniem
+
+	fld qword [esp+12+12]
+	fsub qword [esp+4+12]
+	fdiv qword [dwa]
+	fild dword [esp+20+12]
+	fdiv
+	fmul
+
+    pop esi
+    pop ecx
+    pop eax
+
+	ret
+
--- src/Playground.java
+++ src/Playground.java
@@ -6,8 +6,8 @@ public class Playground {
 //		double result = e.setVariable("x", 0).evaluate();
 //		System.out.println(result);
 
-//		double result = Integrator.integrate(-10, 10, 1000, "2/x");
-//		System.out.printf("%.3f", result);
+		double result = new Integrator().integrateASM(-10, 10, 1000, "1");
+		System.out.printf("%.3f", result);
 
 
 
