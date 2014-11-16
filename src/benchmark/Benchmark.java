9c0cb8a1f7bf962b61f58f7671ad2e4cf35ff035
Aleksander Grzybowski <kelog@orange.pl>
2014-11-16 21:03:38 +0100

    Add simple benchmark

---
+++ src/benchmark/Benchmark.java
@@ -0,0 +1,39 @@
+package benchmark;
+
+import implems.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Benchmark {
+
+	static String functionString = "sin(x)";
+	static double left = 0;
+	static double right = Math.PI * 2;
+	static int points = 1000 * 1000;
+	static int iters = 10;
+
+	static List<Class<? extends Integrator>> implems = new ArrayList<Class<? extends Integrator>>();
+	static {
+		implems.add(CIntegrator.class);
+		implems.add(AsmFPUIntegrator.class);
+		implems.add(AsmSSEIntegrator.class);
+		implems.add(JavaIntegrator.class);
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		for (Class<? extends Integrator> clazz : implems) {
+			Integrator instance = clazz.newInstance();
+			System.out.print("*** Benchmark for " + instance.getClass() + " -> ");
+
+			long sumOfTimes = 0;
+			for (int i = 0; i < iters; ++i) {
+				sumOfTimes += instance.integrate(left, right, points, functionString).timeNS;
+			}
+
+			System.out.println("" + ((double) (sumOfTimes / iters)) / (1000000.0) + " ms\n");
+		}
+	}
+
+}
