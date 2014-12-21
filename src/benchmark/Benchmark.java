3009f0c718b0dbac1edcf7db2350b587e91d7477
Aleksander Grzybowski <kelog@orange.pl>
2014-12-21 15:48:34 +0100

    Benchmark with threads, turn off logging

--- src/benchmark/Benchmark.java
+++ src/benchmark/Benchmark.java
@@ -4,6 +4,7 @@ import implems.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Benchmark {
 
@@ -12,6 +13,7 @@ public class Benchmark {
 	static double right = Math.PI * 2;
 	static int points = 1_000_000;
 	static int iters = 10;
+	static int threads = 1;
 
 	static List<Class<? extends Integrator>> implems = new ArrayList<Class<? extends Integrator>>();
 	static {
@@ -22,16 +24,20 @@ public class Benchmark {
 	}
 
 	public static void main(String[] args) throws Exception {
+		// turn off logging
+		Logger l0 = Logger.getLogger("");
+		l0.removeHandler(l0.getHandlers()[0]);
+
 		for (Class<? extends Integrator> clazz : implems) {
 			Integrator instance = clazz.newInstance();
 			System.out.print("*** Benchmark for " + instance.getClass() + " -> ");
 
 			long sumOfTimes = 0;
 			for (int i = 0; i < iters; ++i) {
-				sumOfTimes += instance.integrate(left, right, points, functionString, 10).timeNS;
+				sumOfTimes += instance.integrate(left, right, points, functionString, threads).timeNS;
 			}
 
-			System.out.println("" + ((double) (sumOfTimes / iters)) / (1000000.0) + " ms\n");
+			System.out.println("" + ((double) (sumOfTimes / iters)) / (1_000_000.0) + " ms\n");
 		}
 	}
 
