dbfbb78c2087dcf980e010c57ee3ff85fea9f1fe
Aleksander Grzybowski <kelog@orange.pl>
2015-01-21 23:00:00 +0100

    Ref IntegratorTest

--- src/org/kelog/japroj/tests/IntegratorTest.java
+++ src/org/kelog/japroj/tests/IntegratorTest.java
@@ -12,29 +12,29 @@ import static org.junit.Assert.assertEquals;
 
 public class IntegratorTest {
 
-	private static final int POINTS = 10_000;
+	private static final int NUMBER_OF_POINTS = 10_000;
 	private static final List<Integer> threadCombinations = Arrays.asList(1, 2, 4);
+	private static final double COMPARISON_DELTA = 0.01;
 
+	@SuppressWarnings("UnusedDeclaration")
 	private enum TestCase {
 
-		T01("sin(x)", 0, Math.PI, 2, POINTS),
-		T02("cos(x)", 0, 20 * Math.PI, 0, POINTS),
-		T03("1", -10, 10, 20, POINTS),
-		T04("x", 0, 1, 0.5, POINTS),
-		T05("x^2 + 1", 0, 2, 14.0 / 3.0, POINTS),
-		T06("2^x", 2, 4, 17.3123, POINTS),
-		T07("x^2 + x^(-2)", 1, 2, 17.0 / 6.0, POINTS);
+		T01("sin(x)", 0, Math.PI, 2),
+		T02("cos(x)", 0, 20 * Math.PI, 0),
+		T03("1", -10, 10, 20),
+		T04("x", 0, 1, 0.5),
+		T05("x^2 + 1", 0, 2, 14.0 / 3.0),
+		T06("2^x", 2, 4, 17.3123),
+		T07("x^2 + x^(-2)", 1, 2, 17.0 / 6.0);
 
-		TestCase(String function, double left, double right, double expected, int points) {
+		TestCase(String function, double left, double right, double expected) {
 			this.left = left;
 			this.right = right;
-			this.points = points;
 			this.function = function;
 			this.expected = expected;
 		}
 
 		private double left, right;
-		private int points;
 		private String function;
 		private double expected;
 	}
@@ -45,25 +45,26 @@ public class IntegratorTest {
 	@Test
 	public void test_all() throws Exception {
 		if (!LOGGING_ENABLED) {
-			Logger l0 = Logger.getLogger("");
-			l0.removeHandler(l0.getHandlers()[0]);
+			disableLogging();
 		}
 
 		for (Integrator implem : Integrator.values()) {
 			for (int numberOfThreads : threadCombinations) {
-				System.out.println("****** Starting test routine for " + implem.getClass() + " (threads: " + numberOfThreads + ") ******");
+				System.out.println("Starting test routine for " + implem.getClass() + " (threads: " + numberOfThreads + ")");
 
 				for (TestCase testCase : TestCase.values()) {
-					double expected = testCase.expected;
-					double actual = implem.integrate(testCase.left, testCase.right,
-							testCase.points, testCase.function, numberOfThreads).result;
+					double actualResult = implem.integrate(testCase.left, testCase.right,
+							NUMBER_OF_POINTS, testCase.function, numberOfThreads).result;
 
-					System.out.println("Expected = " + expected + " actual = " + actual);
-					assertEquals(expected, actual, 0.01);
+					assertEquals(testCase.expected, actualResult, COMPARISON_DELTA);
 				}
-
-				System.out.println("****** Finished test routine for " + implem.getClass() + " ******\n");
 			}
 		}
+	}
+
+	// from SO
+	private void disableLogging() {
+		Logger l0 = Logger.getLogger("");
+		l0.removeHandler(l0.getHandlers()[0]);
 	}
 }
