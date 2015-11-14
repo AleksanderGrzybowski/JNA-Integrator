52bb4902813075c5c678cd9dbef1227710ed7e9f
Aleksander Grzybowski <kelog@orange.pl>
2015-11-14 18:23:40 +0100

    Web server, stub frontend

---
+++ src/main/java/org/kelog/japroj/web/Controller.java
@@ -0,0 +1,60 @@
+package org.kelog.japroj.web;
+
+import com.google.common.collect.Range;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import org.kelog.japroj.core.Integrator;
+import org.kelog.japroj.di.MainModule;
+import org.kelog.japroj.impl.AsmSSEIntegrator;
+import spark.Spark;
+
+import java.io.File;
+
+import static spark.Spark.get;
+import static spark.Spark.halt;
+
+public class Controller {
+
+	private Integrator integrator;
+	
+	@Inject
+	public Controller(AsmSSEIntegrator integrator) {
+		this.integrator = integrator;
+	}
+	
+	public void init() {
+
+		Spark.externalStaticFileLocation(System.getProperty("user.dir") + File.separator + "web-app");
+
+		get("/calculate", (req, res) -> {
+
+			Range<Double> range;
+			String function;
+			int numberOfPoints;
+			
+			try {
+				range = Range.closed(
+						Double.parseDouble(req.queryParams("left")),
+						Double.parseDouble(req.queryParams("right"))
+				);
+				function = req.queryParams("function");
+				numberOfPoints = Integer.parseInt(req.queryParams("numberOfPoints"));
+			} catch (NumberFormatException e) {
+				halt(400);
+				return "";
+			}
+			
+			try {
+				return integrator.integrate(range, numberOfPoints, function, 3);
+			} catch (Exception e) {
+				halt(400);
+				return "";
+			}
+			
+		}, new JsonTransformer());
+	}
+
+	public static void main(String[] args) {
+		Guice.createInjector(new MainModule()).getInstance(Controller.class).init();
+	}
+}
