9f3bae94a745400a303ee8af79bc1b1ff83ed829
Aleksander Grzybowski <kelog@orange.pl>
2015-11-30 21:36:20 +0100

    Extract css file, refactor js

--- web-app/app.js
+++ web-app/app.js
@@ -1,63 +1,70 @@
-function getParams() {
-    return {
-        left: Number($('#left').val()),
-        right: Number($('#right').val()),
-        numberOfPoints: Number($('#points').val()),
-        func: $('#func').val()
-    }
-}
+var app = {
+    actionUrl: '/calculate',
 
-$(function () {
-    var actionUrl = '/calculate';
-
-    $('#calculate').click(function (e) {
-        e.preventDefault();
-
-        var params = getParams();
-        var span = params.right - params.left;
-        
-        if (span < 0) {
-            alert('Invalid range');
-            return;
+    getParamsFromForm: function () {
+        return {
+            left: Number($('#left').val()),
+            right: Number($('#right').val()),
+            numberOfPoints: Number($('#points').val()),
+            func: $('#func').val()
         }
+    },
 
-        functionPlot({
-            width: $('#plot-container').width(),
-            height: $(window).height() / 2, // TODO better?
-            target: '#plot',
-            xDomain: [params.left - 1, params.right + 1],
-            data: [
-                { // definite integral
-                    fn: params.func,
-                    range: [params.left, params.right],
-                    closed: true,
-                    yDomain: [-1, 1],
-                    xDomain: [-10, 10]
-                },
-                { // outline
-                    fn: params.func,
-                    range: [params.left - span, params.right + span],
-                    yDomain: [-1, 1],
-                    xDomain: [-10, 10]
-                }
-            ]
-        });
+    toLatex: function (params, result) {
+        return "$$ \\int_{" + params.left + "}^{" + params.right + "}" + params.func + " = " + result + " $$";
+    },
 
-        $.ajax({
-            url: actionUrl,
-            type: 'GET',
-            data: params,
-            success: function (data) {
-                var $result = $('#result');
-                
-                $result.text("$$ \\int_{" + params.left + "}^{" + params.right + "}" + params.func + " = " + data.result + " $$");
-                $result.removeClass('animate-fadeInOut');
-                MathJax.Hub.Typeset();
-            },
-            error: function () {
-                $('#result').text('Cannot perform calculation.');
+    init: function () {
+        $('#calculate').click(function (e) {
+            e.preventDefault();
+
+            var params = app.getParamsFromForm();
+            var span = params.right - params.left;
+
+            if (span < 0) {
+                alert('Invalid range'); // TODO 
+                return;
             }
-        })
-    });
-});
 
+            functionPlot({
+                width: $('#plot-container').width(),
+                height: $(window).height() / 2, // TODO
+                target: '#plot',
+                xDomain: [params.left - 1, params.right + 1],
+                data: [
+                    { // definite integral
+                        fn: params.func,
+                        range: [params.left, params.right],
+                        closed: true,
+                        yDomain: [-1, 1],
+                        xDomain: [-10, 10]
+                    },
+                    { // outline
+                        fn: params.func,
+                        range: [params.left - span, params.right + span],
+                        yDomain: [-1, 1],
+                        xDomain: [-10, 10]
+                    }
+                ]
+            });
+
+            $.ajax({
+                url: app.actionUrl,
+                type: 'GET',
+                data: params,
+                success: function (data) {
+                    var $result = $('#result');
+
+                    $result.text(app.toLatex(params, data.result));
+                    $result.removeClass('animate-fadeInOut');
+                    MathJax.Hub.Typeset();
+                },
+                error: function () {
+                    $('#result').text('Cannot perform calculation.');
+                }
+            })
+        });
+    }
+};
+
+$(app.init);
