4d3d846139620c735efd4acd30e0b76f3e254371
Aleksander Grzybowski <kelog@orange.pl>
2017-08-15 15:58:45 +0200

    First step in porting to Spring Boot

---
+++ src/main/java/org/kelog/japroj/JnaIntegratorApplication.java
@@ -0,0 +1,12 @@
+package org.kelog.japroj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnaIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnaIntegratorApplication.class, args);
+	}
+}
